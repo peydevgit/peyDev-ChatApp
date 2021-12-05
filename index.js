d3778be9f5a75ff7ae8bcdddba0fabbbc74dc131
peyDEV <93034361+peydevgit@users.noreply.github.com>
2021-12-05 12:48:13 +0100

    revert back to another commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.use(session({
     secret: 'secret',
@@ -21,118 +21,112 @@ app.use(session({
 const users = (JSON.parse(fs.readFileSync('user.json')));
 console.log(users);
 
+let sessionUsername = '';
+let sessionLoggedin = false;
 
 
-
-// //////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/login.html');
-
+    if (req.session.loggedIn == true)
+        res.sendFile(__dirname + '/public/chat.html');
+    else
+        res.sendFile(__dirname + "/public/login.html");
 });
 
 
-// //////////////////////////////////////////////////////////////////////////////
-// app.post('/login', async function (req, res) {
-//     let userAccount = users.find(user => req.body.username == user.username);
-//     if (userAccount) {
-//         let userPassword = userAccount.password
-//         if (userPassword == req.body.password) {
-//             req.session.loggedIn = true;
-//             const sessionUsername = userAccount.username;
-//             console.log(`${req.session.username} has logged in successfully.`);
-//             return res.redirect('/') + sessionUsername;
-//         }
+//////////////////////////////////////////////////////////////////////////////
+app.post('/login', async function (req, res) {
+    let userAccount = users.find(user => req.body.username == user.username);
+    if (userAccount) {
+        let userPassword = userAccount.password
+        if (userPassword == req.body.password) {
+            req.session.loggedIn = true;
+            req.session.username = req.body.username;
+            sessionUsername = req.session.username;
+            sessionLoggedin = req.session.loggedIn;
 
-//     }
-//     else {
-//         console.log(`${req.body.username} has not been found in the database.`)
-//         res.redirect('/');
-//     }
-// });
-
-
-
-// /////register////////////////////////////////////////////////////////////////
-// app.get('/register', (req, res) => {
-//     if (req.session.loggedIn == true) {
-//         res.sendFile(__dirname + '/public/chat.html');
-//     }
-//     else {
-//         res.sendFile(__dirname + '/public/register.html')
-//     }
-// });
-
-
-// ////register post/////////////////////////////////////////////////////////
-// app.post('/register', async (req, res) => {
-//     let userAccount = users.find((data) => data.username == req.body.username);
-//     console.log(req.body.username);
-//     if (!userAccount) {
-//         let newUser = {
-//             userID: '',
-//             username: req.body.username,
-//             password: req.body.password,
-//         }
-//         users.push(newUser);
-//         fs.writeFileSync('user.json', JSON.stringify(users, null, 4))
-//         res.redirect('/')
-//         console.log(`User ${newUser.username} has been registered successfully.`)
-//     }
-//     else {
-//         res.redirect('/register')
-//         console.log(`${req.body.username} has already been registered`);
-//     }
-// });
-
-
-
-// // Logs out a user./////////////////////////////////////////////
-// app.get('/logout', function (req, res) {
-//     console.log(`${req.session.username} has been logged out.`)
-//     req.session.destroy;
-//     req.session.loggedIn = false;
-//     sessionLoggedin = false;
-//     res.redirect('/');
-// });
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////
-
-io.on('connection', (socket) => {
-    
-    sessionID = socket.id;
-    socket.user = 'test';
-    console.log(`User ${socket.user} with userID: ${sessionID} (copied from new socket.id) connected`);
-    socket.on('connect', () => {
-    text = `${socket.user} has connected with ${sessionID}`
-        socket.broadcast.emit('connecting', text);
-    });
-});
-
-io.on('login', (username, password) => {
-        let userAccount = users.find(user => user.username == username && user.password == password)
-        if (userAccount == true) {
-            console.log('Konto hittad.')
+            console.log(`${req.session.username} has logged in successfully.`);
+            return res.redirect('/');
         }
-        else {
-            console.log('konto inte hittad.')
-        }
-  
+        
+    }
+    else {
+        console.log(`${req.body.username} has not been found in the database.`)
+        res.redirect('/');
+    }
 });
+
+
+
+/////register////////////////////////////////////////////////////////////////
+app.get('/register', (req, res) => {
+    if (req.session.loggedIn == true) {
+        res.sendFile(__dirname + '/public/chat.html');
+    }
+    else {
+        res.sendFile(__dirname + '/public/register.html')
+    }
+});
+
+
+////register post/////////////////////////////////////////////////////////
+app.post('/register', async (req, res) => {
+    let userAccount = users.find((data) => data.username == req.body.username);
+    console.log(req.body.username);
+    if (!userAccount) {
+        let newUser = {
+            userID: '',
+            username: req.body.username,
+            password: req.body.password,
+        }
+        users.push(newUser);
+        fs.writeFileSync('user.json', JSON.stringify(users, null, 4))
+        res.redirect('/')
+        console.log(`User ${newUser.username} has been registered successfully.`)
+    }
+    else {
+        res.redirect('/register')
+        console.log(`${req.body.username} has already been registered`);
+    }
+});
+
+
+
+// Logs out a user./////////////////////////////////////////////
+app.get('/logout', function (req, res) {
+    console.log(`${req.session.username} has been logged out.`)
+    req.session.destroy;
+    req.session.loggedIn = false;
+    sessionLoggedin = false;
+    res.redirect('/');
+});
+
+
+app.get('/getusername', (req, res) => {
+    res.send(req.session.username)
+});
+
+
+// När en användare ansluter eller disconnectar.///////////////
 io.on('connection', (socket) => {
-    sessionID = socket.id;
-    socket.user = 'sessionUsername';
+    if (sessionLoggedin == true) {
+        let userAccount = users.find((data) => data.username == sessionUsername);
+        userAccount.userID = socket.id;
+        socket.user = userAccount.username;
+        console.log(`User ${socket.user} with userID: ${userAccount.userID} (copied from new socket.id) connected`);
+        text = `${socket.user} has connected.`
+        io.emit('connecting', text);
+ 
+        
+        
+
+   
+    }
+
     socket.on('disconnect', () => {
-        userDisconnected = `${socket.user} has disconnected`
-        socket.broadcast.emit('disconnected', userDisconnected);
+        console.log('user disconnected');
     });
 });
-
 
 
 /////medelande till alla////////////////////////////////////////
