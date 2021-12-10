c436e7f19e0add7d95cb2467fee411cb287cfc01
peyDEV <93034361+peydevgit@users.noreply.github.com>
2021-12-10 23:15:42 +0100

    lagt till svenska kommentarer
    final versionen med kommentarer.

--- public/script/script.js
+++ public/script/script.js
@@ -1,43 +1,43 @@
 window.onload = function() {
 
-    const socket = io.connect()
+    const socket = io.connect() // anropar socket io konstruktor
 
-    let socketUser;
-    // declaring variabels for html elements.
+    let socketUser; // vi kommer använda denna som klientens användarnamn så den får en tilldelning sen.
+    // deklarerar html elementen som variabler.
     let messages = document.getElementById('messages');
     let input = document.getElementById('input');
     let userOnline = document.getElementById('online_user')
     let chat_send = document.getElementById('chat_send');
 
 
-    let userOnlineList = (username) => {
-            let online_user_list = document.createElement('li');
-            online_user_list.id = username;
-            online_user_list.textContent = username;
-            userOnline.appendChild(online_user_list);
-            window.scrollTo(0, document.body.scrollHeight);
+    let userOnlineList = (username) => { // skapar en funktion som vi kan mata in klient användarnamn  som sedan skapar ett element för online listan.
+            let online_user_list = document.createElement('li'); // skapar list element och till ger den variabel för att förenkla.
+            online_user_list.id = username; // vi ger id attributen klientens användarnamn för lättare borttagning.
+            online_user_list.textContent = username; // och det ska stå deras namn i list elementet som skapas.
+            userOnline.appendChild(online_user_list); //  vi appendar den till useronline elementet.
+            window.scrollTo(0, document.body.scrollHeight); // ifall den når slutet av sidans ram så ska det skapas en scrolll.
         }
         // vi skapar en validator funktion för onskefulla input för att slippa kopiera och klistra det överallt som behövs.
     let valiDator = (input) => {
         let output = '';
-        for (let i = 0; i < input.length; i++) {
-            if (input[i] == '<')
-                output += '&lt;';
-            else
+        for (let i = 0; i < input.length; i++) { // den ska for loopas till inputens längd.
+            if (input[i] == '<') // ifall någon av bokstäverna är lika med krokodilkäft
+                output += '&lt;'; // lägg till en escape sekvens istället
+            else // annars accepteras bokstaven och läggs till den nya innehållet.
                 output += input[i];
         }
-        return output;
+        return output; // returnerar den igenomsökta texten.
     }
 
-    socket.on('get online users', (onlineUsers) => {
-        userOnline.innerHTML = '';
-        console.log(onlineUsers)
-        for (i = 0; i < onlineUsers.length; i++) {
-            userOnlineList(onlineUsers[i]);
+    socket.on('get online users', (onlineUsers) => { // när vi får en serverknackning.
+        userOnline.innerHTML = ''; // vi nollställer useronline elementet och tar bort alla i online listan, ifall de finns de som är offline med.
+        console.log(onlineUsers) // feedback så vi ser att listan är ny med inga disconnectade anvöändare
+        for (i = 0; i < onlineUsers.length; i++) { // vi kör en klassisk for loop
+            userOnlineList(onlineUsers[i]); // lägger varje användare i useronlinelist funktionen som skapar element för online användare.
         }
     })
 
-    socket.on('update users online', (onlineUsers) => {
+    socket.on('update users online', (onlineUsers) => { //  när någon har disconnectat vill vi ha en uppdaterad online lista.
         /////// printing out the new list.
         userOnline.innerHTML = '';
         console.log(onlineUsers)
@@ -49,95 +49,93 @@ window.onload = function() {
     });
 
 
-    let check_user = (username) => {
-        if (!username) {
-            username = prompt('Skriv ett namn');
-            while (username.length < 4)
+    let check_user = (username) => { // skapar en funktion som vi kan mata in användares namn, om användaren inte finns. skapa en och skicka en klient händelse till servern.
+        if (!username) { // om en användare inte finns med namnet som de tätter in med.
+            username = prompt('Skriv ett namn'); // tillger variabeln ett prompt som sedan ger den ett värde.
+            while (username.length < 4) // så länge de skriver mindre än 4 bokstäver så kör en while loop.
                 username = prompt('Skriv ett längre namn')
-            socket.emit('new user', username)
-            socket.emit('joined chat', username);
+            socket.emit('new user', username) // vi skickar händelsen till servern om att en ny användare har skapats.
+            socket.emit('joined chat', username); // händelse att vi ska medela andra användare att en ny användare har joinat chattet.
         } else
-            alert('Välkommen tillbaks' + username)
+            alert('Välkommen tillbaks' + username) //  denna la jag till ifall jag skulle vilja spara alla användare i en databas.
     }
 
-    check_user(socketUser);
+    check_user(socketUser); // här användaren vi funktionen ovan.
 
 
 
 
-    socket.on('new user', (username, ) => {
-        // when a user has succesfully logged in.
-
-        socket.emit('get online users');
+    socket.on('new user', () => { // vi får en server knackning om att ny användare.
+        socket.emit('get online users'); // vi skickar händelse till servern om att skapa en online lista.
 
     });
 
 
-    socket.on('joined chat', (username) => {
-        let online_user_message = document.createElement('li');
-        online_user_message.textContent = `${username} has joined the chat!`;
-        messages.appendChild(online_user_message);
-        window.scrollTo(0, document.body.scrollHeight);
-        socket.emit('update users online')
+    socket.on('joined chat', (username) => { // vi får händelse från server om att någon har joinat chatt.
+        let online_user_message = document.createElement('li'); // vi skapar en li element.
+        online_user_message.textContent = `${username} has joined the chat!`; // li elementets innehållstext ska vara att användare har joinat ett chat.
+        messages.appendChild(online_user_message); // appendar till message elementet.
+        window.scrollTo(0, document.body.scrollHeight); //  scrollbar om de mkt text medelanden.
+        socket.emit('update users online') // uppdaterar online listan.
     });
 
 
-    socket.on('user disconnected', (username, users_online) => {
-        let user_disconnect_message = document.createElement('li');
-        user_disconnect_message.textContent = `${username} disconnected`
-        messages.appendChild(user_disconnect_message);
+    socket.on('user disconnected', (username, users_online) => { // när någon avslutar chattappen, så får vi server knackning med datan.
+        let user_disconnect_message = document.createElement('li'); // skapar en li element.
+        user_disconnect_message.textContent = `${username} disconnected` // li elementets innehåll.
+        messages.appendChild(user_disconnect_message); // appendar till message elementet.
         window.scrollTo(0, document.body.scrollHeight);
-        console.log(`${username} on online list is removed.`)
-        console.log(users_online)
-        socket.emit('update users online', users_online);
+        console.log(`${username} on online list is removed.`) // feedback
+        console.log(users_online) // feedback
+        socket.emit('update users online', users_online); // uppdaterar online list genom klient knackning
     })
 
 
 
-    chat_send.addEventListener('click', function(e) {
-        input.value = valiDator(input.value)
-        e.preventDefault();
-        let savedMessage = input.value;
-        if (input.value.length > 0) {
-            socket.emit('chat message', input.value);
-            input.value = '';
-            userTyping.value = '';
-            let youMessage = document.createElement('li') // rensa inmatningsfältet
-            let savedText = `You: ${savedMessage}`;
-            youMessage.id = "youWrote";
-            youMessage.innerHTML = savedText;
-            messages.appendChild(youMessage);
+    chat_send.addEventListener('click', function(e) { // lyssnare på button knappen chat_send
+        input.value = valiDator(input.value) // det som matas in söker vi igenom genom funktionen för onskefulla input.
+        e.preventDefault(); // att sidan inte ska laddas om.
+        let savedMessage = input.value; // sparar inputen.. kanske inte behövs efter man har gått igenom koden :)
+        if (input.value.length > 0) { // om medelandet är större än 0.. dvs räcker med en bokstav.
+            socket.emit('chat message', input.value); // en händelse till servern med innehållet.
+            input.value = ''; // nollställer inputen
+            userTyping.value = ''; // nollställer user is typing.
+            let youMessage = document.createElement('li') // skapar en ny element eftersom vi kör broadcast och det betyder att användaren som skriver något kan inte se sitt eget medelande.
+            let savedText = `You: ${savedMessage}`; // tillger den till savedText, att du: skrev något.
+            youMessage.id = "youWrote"; // id attributen till elementet..
+            youMessage.innerHTML = savedText; // elementets innehålls html
+            messages.appendChild(youMessage); // appendar till message.
             window.scrollTo(0, document.body.scrollHeight);
-
-
         }
     });
 
 
-    socket.on('chat message', function(msg) {
-        let online_user_message = document.createElement('li');
-        online_user_message.textContent = msg;
-        messages.appendChild(online_user_message);
-        userTyping.value = '';
+    socket.on('chat message', function(msg) { // servern skickar information för att kunna skapa ett medelande.
+        let online_user_message = document.createElement('li'); // skapar element
+        online_user_message.textContent = msg; // elementets textinnehåll
+        messages.appendChild(online_user_message); // appendar till message stället.
+        userTyping.value = ''; // nollställer user is typing.
         window.scrollTo(0, document.body.scrollHeight);
     });
 
-    input.addEventListener('keyup', () => {
-        socket.emit('typing')
+    input.addEventListener('keydown', () => { // en lyssnare som triggas när en tangentbord trycks ner.
+        socket.emit('typing') // vi skickar en klient händelse om att någon skriver nåt.
     });
-    input.addEventListener('keydown', () => {
-        socket.emit('not typing')
+
+    input.addEventListener('keyup', () => { // lyssnare som triggas när något trycks up i input fältet.
+        socket.emit('not typing') // vi skickar en klient händelse om att någon inte skriver nåt.
     });
+
 
     socket.on('typing', (socket) => {
+        userTyping.value = `${socket}  is typing....`; // elementet värde är dess användare som skriver.
 
-        userTyping.value = '';
+
 
     });
 
-    socket.on('not typing', (socket) => {
-        userTyping.value = `${socket}  is typing....`;
-
+    socket.on('not typing', () => { // händelse från servern om att användaren slutat skriva.
+        userTyping.value = ''; // vi lägger elementet tom.
     });
 
 
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require('express'); // importerar express
 const app = express();
-const http = require('http');;
+const http = require('http');; // importerar http
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
     console.log(`Server startad på port: ${PORT}`);
 });
@@ -15,61 +16,58 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //////////////////////////////////////////////////////////////////////////////
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
+app.get('/', function(req, res) { // Vi definierar routern '/' som blir anropad när en person besöker sidan.
+    res.sendFile(__dirname + "/public/index.html"); // skickar responses att vi skickar dom till index.html
 });
 
 
 
 
+let users_online = [] // vi skapar ett objekt
+let removedUser; // vi kommer använda detta senare för när vi raderar en användare.
 
-let users_online = []
-let removedUser;
-// När en användare ansluter eller disconnectar.///////////////
-io.on("connection", (socket) => {
-    socket.on('new user', (username) => {
-        socket.user = username;
-        users_online.push(socket.user);
-        io.emit("new user", username, users_online);
-        console.log(socket.user + ' har skapats.');
-        console.log(users_online)
+io.on("connection", (socket) => { // när en klient ansluter 'connection' och 'socket' är klienten. den visar den aktuella händelsen eller en klient knackning.
+    socket.on('new user', (username) => { // vi tar emot en knackning från klienten om att vi har fått en ny användare.
+        socket.user = username; // vi tillger socket variabeln som tillhör chat modulen ett attribute som user och att den är lika med det klienten fått som inmatning
+        users_online.push(socket.user); // Lägger till den nya användaren in i objektet.
+        io.emit("new user", username, users_online); // vi skickar till alla att vi har fått en ny användare. en server knackning kan man kalla det.
+        console.log(socket.user + ' har skapats.'); // console feedback för servern.
+        console.log(users_online) // console feedback för servern. Där vi ser hur många användare som finns.
     });
 
-    ///////// when someone disconnects
-    socket.on('disconnect', () => {
-        removedUser = socket.user
-        users_online = users_online.filter(item => item != socket.user);
-        socket.broadcast.emit('user disconnected', removedUser, users_online);
+
+    socket.on('disconnect', () => { ///////// När en klient lämnar chattet. en klientknackning.
+        removedUser = socket.user // vi sparar klientens namn som removedUser variabeln.
+        users_online = users_online.filter(item => item != socket.user); // vi tar bort klienten från listan
+        socket.broadcast.emit('user disconnected', removedUser, users_online); // Vi skickar till resten av klienterna som är inloggade.
     });
 
-    //// when someones log in, it will be broadcasted to other clients .
-    socket.on('joined chat', (username) => {
 
-        socket.broadcast.emit('joined chat', username);
+    socket.on('joined chat', (username) => { // vi får en knackning från klienten att vi ska skriva ett medelande i chattet att 'Användare X' har anslutit.
+        socket.broadcast.emit('joined chat', username); // vi skickar en broadcast som betyder att alla får detta förutom den klienten som har loggat in precis.
     });
 
-    ////////// when someone writes something.
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', `${socket.user}: ${msg}`);
-    });
-    ///////////////////////////////////////////
-    socket.on('typing', () => {
-        socket.broadcast.emit('typing', socket.user);
+
+    socket.on('chat message', (msg) => { // Chatt message får vi när någon försöker skriva ett medelande i chattet. en klientknackning.
+        socket.broadcast.emit('chat message', `${socket.user}: ${msg}`); // vi broadcastar innehållet till alla förutom den som skrev det. - en serverknackning till klienten
     });
 
-    socket.on('not typing', () => {
+    socket.on('typing', () => { // en knackning från från klienten när någon börjar skriva i input fältet.
+        socket.broadcast.emit('typing', socket.user); // vi skickar tillbaks med en serverknackning
+    });
+
+    socket.on('not typing', () => { // här är när de inte skriver något..  eller har helt enkelt slutat skriva efter de skrev något.
         socket.broadcast.emit('not typing', socket.user);
     });
     ////////////////////////////////77
 
-    socket.on('get online users', () => {
-        //Send over the onlineUsers
-        socket.broadcast.emit('get online users', users_online);
+    socket.on('get online users', () => { /// efter de har loggat in så skickar klienten en händelse knackning på att den vill ha online listan.
+        socket.broadcast.emit('get online users', users_online); // vi skickar online listan .
     })
 
 
-    socket.on('update online users', (useronline) => {
-        socket.broadcast.emit('get online users', useronline);
+    socket.on('update online users', (useronline) => { // denna knackning sker från klienten ifall någon disconnectar, då vill vi ha en ny lista där den som har stängt ner chatten inte finns längre.
+        socket.broadcast.emit('get online users', useronline); // vi skickar den nya listan.
     })
 
 });
